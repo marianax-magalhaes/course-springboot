44e4d9cbc37bf7983cd4df0ceb7da13c3e9e7291
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-02-15 11:33:28 -0300

    perfil test config

--- src/main/java/com/mariana/course/entities/User.java
+++ src/main/java/com/mariana/course/entities/User.java
@@ -4,14 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class User implements Serializable {
 
+    @Id
     private Long id;
+
     private String name;
     private String email;
     private String phone;
