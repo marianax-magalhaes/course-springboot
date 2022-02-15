b776c7819ead666bc126185fe87426000d8ccd5c
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-02-15 10:56:59 -0300

    adicao do lombok

---
+++ src/main/java/com/mariana/course/entities/User.java
@@ -0,0 +1,12 @@
+package com.mariana.course.entities;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+}
