bcd40645d242af541f94b9a2a5141201fcbcdd6f
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-02-15 12:10:15 -0300

    service de user com find by all e by id

---
+++ src/main/java/com/mariana/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.mariana.course.services;
+
+import com.mariana.course.entities.User;
+import com.mariana.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repo.findById(id);
+        return obj.get();
+    }
+}
