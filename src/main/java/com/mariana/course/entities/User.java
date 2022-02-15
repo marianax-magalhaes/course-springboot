6c5a1211bb818d83cd41f6695ede0754678d39e0
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-02-15 11:19:57 -0300

    entidade e resource user

--- src/main/java/com/mariana/course/entities/User.java
+++ src/main/java/com/mariana/course/entities/User.java
@@ -1,9 +1,16 @@
 package com.mariana.course.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
+@Data
 @NoArgsConstructor
-public class User {
+@AllArgsConstructor
+public class User implements Serializable {
+
     private Long id;
     private String name;
     private String email;
