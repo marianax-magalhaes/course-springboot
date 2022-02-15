aab4193fca563d90b65905d6bc40d46fdeb3cb54
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-02-15 10:52:29 -0300

    1o commit

---
+++ src/test/java/com/mariana/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariana.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
