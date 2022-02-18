65a2dc3a72aa25258a58970aafdfaebf2a37baef
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-02-18 10:45:22 -0300

    ajuste jsonignore

--- src/main/java/com/mariana/course/entities/Payment.java
+++ src/main/java/com/mariana/course/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.mariana.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
